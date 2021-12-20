308b5ea41827d461c99235e743add100a49deff7
Ankan1998 <ankanroni3@gmail.com>
2021-12-20 23:14:29 +0530

    main.dart file added

--- streams/lib/main.dart
+++ streams/lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+
+import 'main_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,69 +15,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: MainPage(),
     );
   }
 }
 
-class StreamControllerClass{
-  StreamController<int> _controller = StreamController<int>.broadcast();
-  Stream<int> get outpipe => _controller.stream;
-  Sink<int> get inpipe => _controller.sink;
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  StreamControllerClass _ctrlcls = StreamControllerClass();
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body:Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                StreamBuilder<int>(
-                  stream: _ctrlcls._controller.stream,
-                    builder: (BuildContext context,AsyncSnapshot<int> snapshot){
-                      if(!snapshot.hasData){
-                        return Text("Has No data");
-                      }
-                      // Getting number from the stream as comes from stream
-                      return Text(snapshot.data.toString());
-
-                    }
-                ),
-                ElevatedButton(
-                    onPressed: ()async {
-                      // It is sending number one by one to sink at a delay of 1 sec
-                      for(int i=0;i<10;i++){
-                        _ctrlcls._controller.sink.add(i);
-                        await Future.delayed(Duration(seconds: 1));
-                      }
-                    },
-                    child: Text(
-                      "press me"
-                    )
-                )
-              ],
-            ),
-          ),
-        )
-      ),
-    );
-  }
-
-  @override
-  void dispose(){
-    _ctrlcls._controller.close();
-  }
-}
