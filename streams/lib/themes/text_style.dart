69578e790934849c4cb27e39d94601dfbc7e7f8c
Ankan1998 <ankanroni3@gmail.com>
2021-12-21 11:02:27 +0530

    textStyle added

---
+++ streams/lib/themes/text_style.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+class TextStyles{
+  TextStyles._();
+
+  static TextStyle largeTitle = TextStyle(
+      fontFamily: GoogleFonts.poppins().fontFamily,
+      fontWeight: FontWeight.w100,
+      letterSpacing: 2.0,
+      fontSize: 36.0
+  );
+
+  static TextStyle button1 = TextStyle(
+      fontFamily: GoogleFonts.roboto().fontFamily,
+      fontWeight: FontWeight.w200,
+      fontSize: 20.0,
+      color: Colors.grey[100]
+  );
+
+}
